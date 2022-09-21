285adde23f6611b06b9970428adb8aa4b456c31c
sarthakpant772 <sarthak.pant31@gmail.com>
2022-09-21 20:46:30 +0530

    added login

--- server/controller/auth.js
+++ server/controller/auth.js
@@ -17,6 +17,21 @@ const registerUser = async (req, res) => {
   }
 }
 
-const registerCompany = async (req, res) => {}
+const loginUser = async (req, res) => {
+  const body = req.body
+  const email = body?.email
+  const password = body?.password
+  try {
+    const checkUser = await User.findOne({ email: email })
+    if (checkUser.password === password) {
+      res.status(200).json(checkUser)
+    } else {
+      res.status(401).json({ text: 'password not correct' })
+    }
+  } catch (err) {
+    console.log(err)
+    res.status(500).json(err)
+  }
+}
 
-module.exports = { registerUser }
+module.exports = { registerUser, loginUser }
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,9 +1,9 @@
 const express = require('express')
-const { registerUser } = require('../controller/auth')
+const { registerUser, loginUser } = require('../controller/auth')
 const User = require('../model/User')
 
 const router = express.Router()
 
 router.post('/register', registerUser)
-
-module.exports = router;
+router.post('/login', loginUser)
+module.exports = router
