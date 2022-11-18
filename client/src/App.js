8cb156caff2696ddab2809c5beb39021a9856ea5
aasthagarg128 <aasthagarg128@gmail.com>
2022-11-18 20:18:50 +0530

    nav bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,16 @@
 import Navbar from './components/Navbar'
 import Login from './components/Login'
 import './App.css'
+import Productlist from './components/Productlist'
 
 
 
 function App() {
   return (
     <div>
-
-      <Navbar />
-      <Login/>
+      <Navbar/>
+      <Productlist />
+      
     </div>
   )
 }
