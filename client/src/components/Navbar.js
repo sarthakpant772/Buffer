06ae43e1f9d59302cf0d46f7b0afb5d572cdeae8
aasthagarg128 <aasthagarg128@gmail.com>
2022-11-19 20:10:06 +0530

    nav bar completed

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,5 +1,7 @@
 import React from 'react'
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import { Box, Typography } from '@mui/material'
+import { textAlign } from '@mui/system'
 const Navbar = () => {
   return (
     <div>
@@ -11,7 +13,7 @@ const Navbar = () => {
         }}
       >
         {/* upperbox */}
-        <Box sx={{ height: '2.8125em' }}>
+        <Box sx={{ height: '2.8125em' , borderBottom:'solid'}}>
           {/* left */}
           <Box sx={{ display: 'flex', textAlign: 'center' }}>
             <Box
@@ -28,23 +30,145 @@ const Navbar = () => {
             <Box
               sx={{
                 display: 'flex',
+                height: '2.81em',
+                width: '6.25em',
+                alignItems: 'center',
+                justifyContent: 'center',
               }}
             >
               <Typography variant="p"> val1</Typography>
             </Box>
-            <Box>
+            <Box
+              sx={{
+                display: 'flex',
+                height: '2.81em',
+                width: '6.25em',
+                alignItems: 'center',
+                justifyContent: 'center'
+
+              }}>
               <Typography variant="p"> val1</Typography>
             </Box>
-            <Box>
+            
+            <Box
+             sx={{
+                display:'flex',
+                height:'2.81em',
+                width: '6.25em',
+                alignItems:'center',
+                justifyContent:'center'              
+            }}
+            >
               <Typography variant="p"> val1</Typography>
             </Box>
-            <Box>
-              <Typography variant="p"> val1</Typography>
+            <Box
+            sx={{
+              display:'flex',
+              height: '2.81em',
+              width:'6.25em',
+              alignItems:'center',
+              justifyContent:'center',
+              borderRight:'solid'
+            }}
+            >
+              <Typography variant="p"> val3</Typography>
             </Box>
           </Box>
           {/* right:  */}
-          <Box></Box>
+         
+          
         </Box>
+
+
+
+            {/*lower box*/}
+        <Box sx={{ display:'flex',
+          flexDirection:'row',
+          justifyContent:'space-between',
+          borderBottom:'solid' ,
+          width:'100%'    
+        }} 
+          >
+         
+          {/*left box */}
+        <Box sx={{display:'flex',textAlign:'center',width:'70%'
+      }}>
+          <Box
+           sx={{
+            display:'flex',
+            width:'5.88em',
+            height:'5.5em',
+            alignItems:'center',
+            justifyContent:'center',
+            
+
+           }}
+          
+          >
+            <Typography variant='p'>logo</Typography>
+            </Box>  
+
+           <Box
+           sx={{
+            display:'flex',
+            width:'9.8125em',
+            height:'5.5em',
+            alignItems:'center',
+            justifyContent:'center'
+
+           }}>
+            <Typography variant='p'> val1</Typography>
+            </Box>   
+
+          <Box
+          sx={{
+            display:'flex',
+            width:'9.8125em',
+            height:'5.5em',
+            alignItems:'center',
+            justifyContent:'center'
+          }}  >
+
+          <Typography variant='p'>val2</Typography>
+          </Box>
+
+          <Box
+           sx={{
+            display:'flex',
+            width:'9.8125em',
+            height:'5.5em',
+            alignItems:'center',
+            justifyContent:'center'
+
+           }}>
+            <Typography variant='p'> val3</Typography>
+            </Box>  
+
+
+            <Box
+           sx={{
+            display:'flex',
+            width:'9.8125em',
+            height:'5.5em',
+            alignItems:'center',
+            justifyContent:'center',
+            borderRight:'solid',
+            position:'relative',
+            right:' 0.9375em'
+
+           }}>
+            <Typography variant='p'> val4</Typography>
+            </Box>  
+          
+        </Box>
+
+           {/*right box */}
+         <Box>
+          <ShoppingCartIcon></ShoppingCartIcon>
+          </Box>
+
+
+      </Box>
       </Box>
     </div>
   )
