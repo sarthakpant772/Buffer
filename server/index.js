464337800b34e5b39031138e4c4b7460c167f40c
sarthakpant772 <sarthak.pant31@gmail.com>
2023-05-03 17:09:11 +0530

    vercelConfig

--- server/index.js
+++ server/index.js
@@ -46,12 +46,14 @@ app.use('/payment', Payment)
 const PORT = process.env.PORT || 5000
 const MONGO_URL =
   'mongodb+srv://buffer:buffer@cluster0.o7wpovx.mongodb.net/?retryWrites=true&w=majority'
-mongoose.set('bufferCommands', false)
+mongoose.set('bufferCommands', true)
 mongoose.connect(MONGO_URL, () => {
   console.log('databse connected')
   app.listen(PORT, () => {
     console.log('server is running at port 5000')
   })
 })
+
+
 
 
